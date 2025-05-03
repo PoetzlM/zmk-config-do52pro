73de142a462dfc14430b1f48efcd65e081fa00de
PoetzlM <89835279+PoetzlM@users.noreply.github.com>
2025-05-03 10:21:22 +0200

    cleaned repo, fixed image for keymap

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Personal ZMK config for the DO52Pro based on infused-kim/zmk-config
+
+
+
+# github keymap editor
+
+# draw keymaps
+
+```bash
+sudo apt update
+sudo apt install pipx
+pipx ensurepath
+
+pipx install keymap-drawer
+
+```
+
+> https://github.com/caksoylar/keymap-drawer
+
--- keymap_img/update_keymap_img.sh
+++ keymap_img/update_keymap_img.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 echo 'Parsing ZMK keymap...'
-keymap -c keymap_config.yaml parse -c 12 -z ../config/corne.keymap > keymap.yaml
+keymap -c keymap_config.yaml parse -c 12 -z ../config/do52pro_tp.keymap > keymap_do52pro_tp.yaml
 
-# echo '\n\nAdjusting keymap yaml...'
-# ./keymap_img_adjuster.py keymap.yaml
+#echo '\n\nAdjusting keymap yaml...'
+#./keymap_img_adjuster.py do52pro_tp.yaml
 
 echo '\n\nDrawing keymap...'
-keymap -c keymap_config.yaml draw --qmk-keyboard crkbd/rev1 --qmk-layout LAYOUT_split_3x6_3 keymap.yaml -s Base Nav Num Func Sym Adjust > keymap.svg
+keymap -c keymap_config.yaml draw --qmk-info-json ../config/do52pro_tp.json keymap_do52pro_tp.yaml > keymap_do52pro_tp.svg
+#output only specific layer
+#keymap -c keymap_config.yaml draw --qmk-info-json ../config/do52pro_tp.json keymap_do52pro_tp.yaml -s base > keymap.svg
