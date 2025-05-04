4c224ff408b035fdc6a4a817a39835f7e39db1af
PoetzlM <89835279+PoetzlM@users.noreply.github.com>
2025-05-04 10:44:30 +0200

    script to remove header added

--- config/remove_header.cmd
+++ config/remove_header.cmd
@@ -1,2 +1,6 @@
-type do52pro_tp.keymap | findstr /v pointing.h > do52pro_tp.keymap
+type do52pro_tp.keymap | findstr /v pointing.h > do52pro_tp_temp.keymap
+
+del do52pro_tp.keymap 
+move do52pro_tp_temp.keymap do52pro_tp.keymap 
+
 git commit -m "removed pointing.h headder"
